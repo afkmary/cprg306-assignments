2ee259ea605a55f8a5ae75f465dc60ff3aebd21c
afkmary <maryannegarcia07@yahoo.com>
2026-03-25 21:46:42 -0600

    replaced env variables  in firebase and hid them from git

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFcDn505vtG1vk7KZHSSjqi2-Y4gIOp40",
-  authDomain: "cprg306-assignments-10da9.firebaseapp.com",
-  projectId: "cprg306-assignments-10da9",
-  storageBucket: "cprg306-assignments-10da9.firebasestorage.app",
-  messagingSenderId: "832831876150",
-  appId: "1:832831876150:web:812ab4d61fa28cb9227c97"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
